65da0acc9a9937dde041708a73b109ad4800851f
wizard <rumpertumpski@gmail.com>
2021-04-26 20:18:39 -0300

    add spinner loader on search

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -7,16 +7,20 @@ import closeIcon from '@iconify/icons-jam/close'
 import YouTube from '../apis/youtube'
 import Ticketmaster from '../apis/ticketmaster'
 
+import Loader from "react-loader-spinner"
+
 export default class SearchForn extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            term: ''
+            term: '',
+            waiting: { search: false }
         }
     }
 
     search = async () => {
+        this.setState({ waiting: { search: true } })
 
         const responseYouTube = await YouTube.get('/search', {
             params: { q: this.state.term }
@@ -28,6 +32,7 @@ export default class SearchForn extends Component {
         
         this.props.onYouTubeData(responseYouTube.data)
         this.props.onTicketmasterData(responseTicketmaster.data)
+        this.setState({ waiting: { search: false } })
     }
 
     handleSubmit = (e) => {
@@ -44,28 +49,34 @@ export default class SearchForn extends Component {
     }
 
     render() {
+        const isWaitingSearch = this.state.waiting.search
         return (
-            <form className="flex flex-wrap mt-12">
-                <div className="w-full flex rounded-full hover:shadow-md">
-    
-                    <div className="w-1/6 text-xl text-gray-400 text-center border border-gray-300 rounded-l-full border-r-0 p-2">
-                        <InlineIcon className="inline" icon={searchIcon} />
-                    </div>
-    
-                    <input className="w-4/6 text-xl text-gray-600 border-t border-b border-gray-300 focus:outline-none p-2"
-                        placeholder="Search artist or band" type="text" value={this.state.term} onChange={this.typeSearchText} />
-    
-                    <div className="w-1/6 text-3xl text-gray-400 text-center border border-gray-300 rounded-r-full border-l-0 p-1">
-                        {this.state.term.length > 0 &&
-                            <InlineIcon className="inline cursor-pointer" icon={closeIcon} onClick={this.clearSearchText} />
-                        }
-                    </div>
-    
-                </div>
-                <div className="w-full mt-6">
-                    <button type="submit" className="text-lg bg-gray-100 rounded py-2 px-6 text-gray-700 hover:shadow-md focus:outline-none" onClick={this.handleSubmit}>Search</button>
-                </div>
-            </form>
+            <div className="flex justify-center">
+                {isWaitingSearch ?
+                    <Loader className="my-12" type="Oval" color="#ef4444" height={48} width={48} timeout={3000} /> :
+                    <form className="flex flex-wrap mt-12">
+                        <div className="w-full flex rounded-full hover:shadow-md">
+            
+                            <div className="w-1/6 text-xl text-gray-400 text-center border border-gray-300 rounded-l-full border-r-0 p-2">
+                                <InlineIcon className="inline" icon={searchIcon} />
+                            </div>
+            
+                            <input className="w-4/6 text-xl text-gray-600 border-t border-b border-gray-300 focus:outline-none p-2"
+                                placeholder="Search artist or band" type="text" value={this.state.term} onChange={this.typeSearchText} />
+            
+                            <div className="w-1/6 text-3xl text-gray-400 text-center border border-gray-300 rounded-r-full border-l-0 p-1">
+                                {this.state.term.length > 0 &&
+                                    <InlineIcon className="inline cursor-pointer" icon={closeIcon} onClick={this.clearSearchText} />
+                                }
+                            </div>
+            
+                        </div>
+                        <div className="w-full mt-6">
+                            <button type="submit" className="text-lg bg-gray-100 rounded py-2 px-6 text-gray-700 hover:shadow-md focus:outline-none" onClick={this.handleSubmit}>Search</button>
+                        </div>
+                    </form>
+                }
+            </div>            
         )
     }
 }
