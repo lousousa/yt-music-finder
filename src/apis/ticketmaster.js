d03e8f64f8d52519968914d95cc130df812de518
wizard <rumpertumpski@gmail.com>
2021-04-29 12:10:26 -0300

    update prod

--- src/apis/ticketmaster.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import keys from './keys.json'
-
-export default axios.create({
-    baseURL: 'https://app.ticketmaster.com/discovery/v2/',
-    params: {
-        apikey: keys.ticketMasterDiscoveryAPI
-    }
-})
